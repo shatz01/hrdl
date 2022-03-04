b71e9bea05fdf4de6e50d5fe9046b43e997fe6a9
Daniel <danielshats1@gmail.com>
2022-03-04 16:40:00 -0500

    patch level training works

---
+++ resnet_experiment.py
@@ -0,0 +1,28 @@
+import torch
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import WandbLogger
+from src.data_stuff.patch_datamodule import TcgaDataModule
+from src.model_stuff.MyResNet import MyResNet
+
+# --- hypers --- #
+data_dir = '/home/shatz/repos/data/imagenette_tesselated/'
+min_patches_per_patient = 0
+# ------------- #
+
+EXP_NAME = f"imagnette_tesselated_resnet"
+logger=WandbLogger(project="moti_imagenette_tesselated", name=EXP_NAME)
+# logger = TensorBoardLogger("./lightning_logs", name=EXP_NAME)
+
+model = MyResNet()
+dm = TcgaDataModule(data_dir=data_dir, batch_size=64, fast_subset=False, min_patches_per_patient=min_patches_per_patient)
+# class_to_idx = dm.get_class_to_idx_dict()
+
+trainer = Trainer(gpus=1, max_epochs=120,
+        logger=logger,
+        # callbacks=[
+        #     PatientLevelValidation.PatientLevelValidation(),
+        #     LogConfusionMatrix.LogConfusionMatrix(class_to_idx),
+        #     ]
+        )
+
+trainer.fit(model, dm)
---
+++ src/model_stuff/MyResNet.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import torch
+import torchvision
+
+from pytorch_lightning import LightningModule, Trainer
+import torchvision.models as models
+import torchmetrics
+
+class MyResNet(LightningModule):
+    def __init__(self, num_classes=2):
+        super().__init__()
+        self.save_hyperparameters()
+
+        torch_model = models.resnet18(pretrained=True)
+        self.feature_extractor = torch.nn.Sequential(*(list(torch_model.children())[:-1])) # just remove the fc
+        self.fc = torch.nn.Sequential(
+            torch.nn.Linear(512, self.hparams.num_classes),
+            # torch.nn.Sigmoid(),
+        )
+        self.criteria = torch.nn.BCEWithLogitsLoss()
+        # self.criteria = torch.nn.BCELoss()
+
+    def extract_features(self, x):
+        x = self.feature_extractor(x)
+        x = torch.flatten(x, 1) # (batch_sz, 512, 1, 1) -> (batch_sz, 512)
+        return x
+        
+    def forward(self, x):
+        x = self.extract_features(x)
+        x = self.fc(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        # path, x, y = batch
+        x, y = batch
+        out = self(x)
+
+        loss = self.criteria(out, torch.nn.functional.one_hot(y, self.hparams.num_classes).float())
+        acc = torchmetrics.functional.accuracy(torch.argmax(out, dim=1), y)
+        
+        self.log('train_loss', loss, on_step=True, on_epoch=True)
+        self.log('train_acc', acc, on_step=True, on_epoch=True)
+        loss = loss.unsqueeze(dim=-1)
+        return {"loss": loss, "acc": acc, "batch_outputs": out.clone().detach()}
+
+    def validation_step(self, batch, batch_idx):
+        # path, x, y = batch
+        x, y = batch
+        out = self(x)
+
+        
+        # import pdb; pdb.set_trace()
+        val_loss = self.criteria(out, torch.nn.functional.one_hot(y, self.hparams.num_classes).float())
+        val_acc = torchmetrics.functional.accuracy(torch.argmax(out, dim=1), y)
+        
+        self.log('val_loss', val_loss, on_step=True, on_epoch=True)
+        self.log('val_acc', val_acc, on_step=True, on_epoch=True)
+        val_loss = val_loss.unsqueeze(dim=-1)
+        return {"val_loss": val_loss, "val_acc": val_acc, "batch_outputs": out.clone().detach()}
+
+    # on end of train/validation, I can print stuff like this:
+    # print(f"REGULAR train loss: {train_loss} | train acc: {train_acc}")
+    # print(f"REGULAR val loss: {val_loss} | val acc: {val_acc}")
+                
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
