9dc73a8277179030a3e0643271fea8523b8a07bc
shatz <danielshats1@gmail.com>
2022-06-15 18:20:24 +0300

    will have to fix merges

--- src/model_stuff/video_model.py
+++ src/model_stuff/video_model.py
@@ -16,16 +16,37 @@ class ResNetVideo(LightningModule):
         super().__init__()
         self.save_hyperparameters()
 
-        self.model = pytorchvideo.models.resnet.create_resnet(
-                input_channel=3, # RGB input from Kinetics
-                model_depth=50, # For the tutorial let's just use a 50 layer network
-                model_num_class=2, # Kinetics has 400 classes so we need out final head to align
-                norm=nn.BatchNorm3d,
-                activation=nn.ReLU,
+        # self.model = pytorchvideo.models.resnet.create_resnet(
+        #         input_channel=3, # RGB input from Kinetics
+        #         model_depth=50, # For the tutorial let's just use a 50 layer network
+        #         model_num_class=2, # Kinetics has 400 classes so we need out final head to align
+        #         norm=nn.BatchNorm3d,
+        #         activation=nn.ReLU,
+        #         )
+
+        spatial_size = 224
+        temporal_size = 16
+        embed_dim_mul = [[1, 2.0], [3, 2.0], [14, 2.0]]
+        atten_head_mul = [[1, 2.0], [3, 2.0], [14, 2.0]]
+        pool_q_stride_size = [[1, 1, 2, 2], [3, 1, 2, 2], [14, 1, 2, 2]]
+        pool_kv_stride_adaptive = [1, 8, 8]
+        pool_kvq_kernel = [3, 3, 3]
+        head_num_classes = 2
+        self.model = pytorchvideo.models.vision_transformers.create_multiscale_vision_transformers(
+                spatial_size=spatial_size,
+                temporal_size=temporal_size,
+                cls_embed_on=False,
+                use_2d_patch=False,
+                embed_dim_mul=embed_dim_mul,
+                atten_head_mul=atten_head_mul,
+                pool_q_stride_size=pool_q_stride_size,
+                pool_kv_stride_adaptive=pool_kv_stride_adaptive,
+                pool_kvq_kernel=pool_kvq_kernel,
+                head_num_classes=head_num_classes,
                 )
         
-        self.criteria = F.cross_entropy
-        # self.criteria = torch.nn.BCEWithLogitsLoss()
+        # self.criteria = F.cross_entropy
+        self.criteria = torch.nn.BCEWithLogitsLoss()
         # self.criteria = torch.nn.BCELoss()
 
         
@@ -36,7 +57,6 @@ class ResNetVideo(LightningModule):
     def training_step(self, batch, batch_idx):
         img_id, img_paths, y, x = batch
         x = torch.swapaxes(x, 1, 2)
-
         out = self(x)
 
         loss = self.criteria(out, torch.nn.functional.one_hot(y, self.hparams.num_classes).float())
@@ -67,4 +87,4 @@ class ResNetVideo(LightningModule):
     # print(f"REGULAR val loss: {val_loss} | val acc: {val_acc}")
                 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.parameters(), lr=1e-4)
